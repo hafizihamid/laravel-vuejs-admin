541efd4aa65e4d439c8727fd431f4aac69288f89
hafizi hamid <mohamadhafizihamid@gmail.com>
2021-10-02 22:21:33 +0800

    feat: Add template Admin One

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,50 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Processors\AvatarProcessor;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var string[]
+     * @var array
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'email', 'password',
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes that should be hidden for arrays.
      *
      * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password', 'remember_token',
     ];
 
     /**
-     * The attributes that should be cast.
+     * The attributes that should be cast to native types.
      *
      * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function file()
+    {
+        return $this->belongsTo(File::class);
+    }
+
+    public function getAvatarAttribute()
+    {
+        return AvatarProcessor::get($this);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get(
-    '/',
-    function () {
-        return view('welcome');
-    }
-);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/*
+ * Clients management
+ * */
+Route::prefix('/clients')->group(function () {
+    Route::get('/', [\App\Http\Controllers\ClientsController::class, 'index']);
+    Route::get('/{client}', [\App\Http\Controllers\ClientsController::class, 'show']);
+    Route::post('/store', [\App\Http\Controllers\ClientsController::class, 'store']);
+    Route::patch('/{client}', [\App\Http\Controllers\ClientsController::class, 'update']);
+    Route::post('/destroy', [\App\Http\Controllers\ClientsController::class, 'destroyMass']);
+    Route::delete('/{client}/destroy', [\App\Http\Controllers\ClientsController::class, 'destroy']);
+});
+
+/*
+ * Current user
+ * */
+Route::prefix('/user')->group(function () {
+    Route::get('/', [\App\Http\Controllers\CurrentUserController::class, 'show']);
+    Route::patch('/', [\App\Http\Controllers\CurrentUserController::class, 'update']);
+    Route::patch('/password', [\App\Http\Controllers\CurrentUserController::class, 'updatePassword']);
+});
+
+/*
+ * File upload (e.g. avatar)
+ * */
+Route::post('/files/store', [\App\Http\Controllers\FilesController::class, 'store']);
