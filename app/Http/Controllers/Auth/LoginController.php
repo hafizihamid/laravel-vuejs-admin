690a161880885213b1382e14dda849135929ff08
hafizi hamid <mohamadhafizihamid@gmail.com>
2021-10-10 15:52:54 +0800

    fix: Login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,39 +2,56 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Http\Controllers\ApiController;
+use App\Services\MemberService;
+use Illuminate\Http\Request;
+use App\Models\Member;
+use Illuminate\Support\Facades\Auth;
+use Session;
 
-class LoginController extends Controller
+class LoginController extends ApiController
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+    protected $memberService;
 
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        parent::__construct();
+        $this->memberService = new MemberService;
+    }
+
+    public function showLoginPasswordForm()
+    {
+        return view('auth.login');
+    }
+
+    public function submitLoginPasswordForm(Request $request)
+    {
+        $credentials = $request->validate(
+            [
+            'username' => 'required',
+            'password' => 'required',
+            ]
+        );
+
+        $user = Member::where('username', $credentials['username'])->first();
+
+        if (!$user) {
+            return view('auth.login')->withErrors(["errorUsername"=>"Invalid username"]);
+        }
+
+        $data = $this->memberService->login($credentials);
+
+        if ($data['status'] != $this->statusCode['ok']) {
+            return view('auth.login')->withErrors(["error"=>'Invalid username/password combination']);
+        }
+            return redirect()->intended('home');
+    }
+
+    public function logout()
+    {
+        Session::flush();
+        Auth::logout();
+  
+        return Redirect('auth.login');
     }
 }
